0db72535d23424761098253bbd470b8963de9d9b
daideguchi <dd.1107.11107@gmail.com>
2022-08-15 23:55:30 +0900

    220815

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navber from "./components/Navbar";
 import Main from "./components/Main";
 
 function App() {
+  // console.log(window.ethereum)
   return (
     <div className="App">
       <Navber />
--- client/src/context/TransactionContext.js
+++ client/src/context/TransactionContext.js
@@ -1,3 +1,22 @@
 import { ethers } from "ethers";
+import { contractABI, contractAddress } from "../utils/connect";
 
 //スマートコントラクトを取得
+    const getSmartContract = () => {
+        //window.ethereum -> インストールしている状態で見ることができるwindow
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        // イーサを送信し、ブロックチェーン内の状態を変更するために支払うことができます。
+        // これには、アカウントの署名者が必要です...
+        const signer = provider.getSigner();
+
+        const transactionContract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+            );
+
+        console.log(provider, signer, transactionContract);
+
+        return transactionContract;
+    
+    };
