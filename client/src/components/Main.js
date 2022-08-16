eef0851b577ad35f2155f988fca23e9a53b0e4f4
daideguchi <dd.1107.11107@gmail.com>
2022-08-16 23:54:40 +0900

    220816

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,8 +3,22 @@ import { useContext } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 
 const Main = () => {
-  const { name } = useContext(TransactionContext)
-  console.log(name);
+  const { connectWallet, sendTransaction, handleChange, inputFormData } =
+    useContext(TransactionContext);
+  
+  
+  const handleSubmit = () => {
+    // console.log(inputFormData.addressTo);
+ 
+    const { addressTo, amount } = inputFormData;
+    if (addressTo === "" || amount === "") {
+      return;
+    } else {
+      sendTransaction();
+    }
+  };
+
+
   return (
     <div className="mainContainer">
       {/*左側*/}
@@ -12,15 +26,30 @@ const Main = () => {
         <h1 className="title">Crypt Card</h1>
 
         <button type="button">
-          <p className="buttonText">ウォレット連携</p>
+          <p className="buttonText" onClick={connectWallet}>
+            ウォレット連携
+          </p>
         </button>
       </div>
       {/*右側 */}
       <div className="inputContainer">
-        <input type="text" placeholder="アドレス" name="addressTo" />
-              <input type="number" placeholder="通貨(ETH)" name="addressTo" step="0.0001" />
-              <button type="button">送信</button>
-          </div>
+        <input
+          type="text"
+          placeholder="アドレス"
+          name="addressTo"
+          onChange={(e) => handleChange(e, "addressTo")}
+        />
+        <input
+          type="number"
+          placeholder="通貨(ETH)"
+          name="addressTo"
+          step="0.0001"
+          onChange={(e) => handleChange(e, "amount")}
+        />
+        <button type="button" onClick={handleSubmit}>
+          送信
+        </button>
+      </div>
     </div>
   );
 }
--- client/src/context/TransactionContext.js
+++ client/src/context/TransactionContext.js
@@ -1,9 +1,11 @@
 import { ethers } from "ethers";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { contractABI, contractAddress } from "../utils/connect";
 
 
 export const TransactionContext = createContext();
+
+const { ethereum } = window;
 
 //スマートコントラクトを取得
     const getSmartContract = () => {
@@ -26,9 +28,83 @@ export const TransactionContext = createContext();
     };
 
 export const TransactionProvider = ({ children }) => {
+    const [currentAccount, setCurrentAccount] = useState("");
+    const [inputFormData, setInputFormData] = useState({
+        addressTo: "",
+        amount: "",
+    });
+
+    const handleChange = (e, name) => {
+        setInputFormData((prevInputFormData) => ({
+            ...prevInputFormData,
+            [name]: e.target.value,
+        }));
+    };
+
+    //メタマスクウォレットと連携しているのかをまずは確認しよう
+    const checkIfWalletIsConnected = async () => {
+      if (!ethereum) return alert("メタマスクをインストールしてください");
+
+      //メタマスクのアカウントIDを取得
+      const accounts = await ethereum.request({ method: "eth_account" });
+      console.log(accounts);
+    };
+
+
+    //メタマスクと連携
+    const connectWallet = async () => {
+        if (!ethereum) return alert("メタマスクをインストールしてください");
+
+        //メタマスクを持っていれば接続を開始する
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        console.log(accounts[0]);
+
+        setCurrentAccount(accounts[0]);
+    };
+
+    //実際に通貨のやり取りをする
+    const sendTransaction = async () => {
+        if (!ethereum) return alert("メタマスクをインストールしてください");
+        console.log("sendTransaction");
+
+        const { addressTo, amount } = inputFormData;
+        const transactionContract = getSmartContract();
+        const parseAmount = ethers.utils.parseEther(amount);
+
+
+
+        const transactionParameters = {
+          gas: "0x2710", 
+          to: addressTo,
+          from: currentAccount,
+          value: parseAmount._hex,
+        };
+
+        const txHash = await ethereum.request({
+          method: "eth_sendTransaction",
+          params: [transactionParameters],
+        });
+
+        const transactionHash = await transactionContract.addToBlockChain(
+          addressTo,
+          parseAmount
+        );
+        console.log(`ロード中・・・${transactionHash.hash}`);
+        await transactionHash.wait();
+        console.log(`送金に成功！${transactionHash.hash}`);
+    };
+
+
+    //reactのhooks 第二引数に[]依存環境を示す。関数が更新した時だけコールバック
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
     return (
-        <TransactionContext.Provider value={{ name: "dd1107" }}>
-            {children}
-        </TransactionContext.Provider>
+      <TransactionContext.Provider
+        value={{ connectWallet, sendTransaction, handleChange, inputFormData }}
+      >
+        {children}
+      </TransactionContext.Provider>
     );
 };
